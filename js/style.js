7fde1460aecdfacf8955521a8396030e1ffc69cf
YuraMyasnikovTrue <my@true-studio.ru>
2021-05-07 18:33:04 +0300

    v4

--- js/style.js
+++ js/style.js
@@ -1,5 +1,355 @@
 $(document).ready(function () {
 
+    $('.psyhBlock .bottom .bottom__list .bottom__list__item:nth-child(1) .item--name--right').hover(
+        function () {
+            $('.info__r1,.info__r2,.info__r3,.info__r4,.info__r5,.info__r6,info-arrow').css({
+                display:"none"
+            })
+        $('.psyhBlock .info-r').css({
+            display:"block",
+            transition:".3s ease"
+        })
+        $('.psyhBlock .info-arrow').css({
+            right:"-19px"
+        })
+        $('.psyhBlock .info-r').css({
+            display:"flex",
+            transform: "scale(1,1) translate(200px, 17px)",
+        })
+        $('.psyhBlock .info-r .info__r1').css({
+            display:"block",
+
+        })
+    }, function () {
+            $('.psyhBlock .info-arrow').css({
+                right:"-19px"
+            })
+            $('.psyhBlock .info-r').css({
+                display:"flex",
+                transform: "scale(0,0) translate(200px, 17px)"
+            })
+    })
+    $('.psyhBlock .bottom .bottom__list .bottom__list__item:nth-child(2) .item--name--right').hover(
+        function () {
+            $('.info__r1,.info__r2,.info__r3,.info__r4,.info__r5,.info__r6,info-arrow').css({
+                display:"none"
+            })
+            $('.psyhBlock .info-r').css({
+                display:"block",
+                transition:".3s ease"
+            })
+            $('.psyhBlock .info-arrow').css({
+                right:"-19px"
+            })
+            $('.psyhBlock .info-r').css({
+                display:"flex",
+                transform: "scale(1,1) translate(200px, 27%)",
+            })
+            $('.psyhBlock .info-r .info__r2').css({
+                display:"block",
+
+            })
+        }, function () {
+            $('.psyhBlock .info-arrow').css({
+                right:"-19px"
+            })
+            $('.psyhBlock .info-r').css({
+                display:"none",
+                transform: "scale(0.3,0.3) translate(200px, 27%)"
+            })
+        })
+    $('.psyhBlock .bottom .bottom__list .bottom__list__item:nth-child(3) .item--name--right').hover(
+        function () {
+            $('.info__r1,.info__r2,.info__r3,.info__r4,.info__r5,.info__r6,info-arrow').css({
+                display:"none"
+            })
+            $('.psyhBlock .info-r').css({
+                display:"block",
+                transition:".3s ease"
+            })
+            $('.psyhBlock .info-arrow').css({
+                right:"-19px"
+            })
+            $('.psyhBlock .info-r').css({
+                display:"flex",
+                transform: "scale(1,1) translate(200px, 46%)",
+            })
+            $('.psyhBlock .info-r .info__r3').css({
+                display:"block",
+
+            })
+        }, function () {
+            $('.psyhBlock .info-arrow').css({
+                right:"-19px"
+            })
+            $('.psyhBlock .info-r').css({
+                display:"flex",
+                transform: "scale(0,0) translate(200px, 46%)"
+            })
+        })
+    $('.psyhBlock .bottom .bottom__list .bottom__list__item:nth-child(4) .item--name--right').hover(
+        function () {
+            $('.info__r1,.info__r2,.info__r3,.info__r4,.info__r5,.info__r6').css({
+                display:"none"
+            })
+            $('.psyhBlock .info-r').css({
+                display:"block",
+                transition:".3s ease"
+            })
+            $('.psyhBlock .info-arrow').css({
+                right:"-19px"
+            })
+            $('.psyhBlock .info-r').css({
+                display:"flex",
+                transform: "scale(1,1) translate(200px, 65%)",
+            })
+            $('.psyhBlock .info-r .info__r4').css({
+                display:"block",
+
+            })
+        }, function () {
+            $('.psyhBlock .info-arrow').css({
+                right:"-19px"
+            })
+            $('.psyhBlock .info-r').css({
+                display:"flex",
+                transform: "scale(0,0) translate(200px, 65%)"
+            })
+        })
+    $('.psyhBlock .bottom .bottom__list .bottom__list__item:nth-child(5) .item--name--right').hover(
+        function () {
+            $('.info__r1,.info__r2,.info__r3,.info__r4,.info__r5,.info__r6').css({
+                display:"none"
+            })
+            $('.psyhBlock .info-r').css({
+                display:"block",
+                transition:".3s ease"
+            })
+            $('.psyhBlock .info-arrow').css({
+                right:"-19px"
+            })
+            $('.psyhBlock .info-r').css({
+                display:"flex",
+                transform: "scale(1,1) translate(200px, 84%)",
+            })
+            $('.psyhBlock .info-r .info__r5').css({
+                display:"block",
+
+            })
+        }, function () {
+            $('.psyhBlock .info-arrow').css({
+                right:"-19px"
+            })
+            $('.psyhBlock .info-r').css({
+                display:"flex",
+                transform: "scale(0,0) translate(200px, 84%)"
+            })
+        })
+    $('.psyhBlock .bottom .bottom__list .bottom__list__item:nth-child(6) .item--name--right').hover(
+        function () {
+            $('.info__r1,.info__r2,.info__r3,.info__r4,.info__r5,.info__r6,info-arrow').css({
+                display:"none"
+            })
+            $('.psyhBlock .info-r').css({
+                display:"block",
+                transition:".3s ease"
+            })
+            $('.psyhBlock .info-arrow').css({
+                right:"-19px"
+            })
+            $('.psyhBlock .info-r').css({
+                display:"flex",
+                transform: "scale(1,1) translate(200px, 103%)",
+            })
+            $('.psyhBlock .info-r .info__r6').css({
+                display:"block",
+
+            })
+        }, function () {
+            $('.psyhBlock .info-arrow').css({
+                right:"-19px"
+            })
+            $('.psyhBlock .info-r').css({
+                display:"flex",
+                transform: "scale(0,0) translate(200px, 103%)"
+            })
+        })
+
+    $('.psyhBlock .bottom .bottom__list .bottom__list__item:nth-child(1) .item--name--left').hover(
+        function () {
+            $('.info__l1,.info__l2,.info__l3,.info__l4,.info__l5,.info__l6,info-arrow').css({
+                display:"none"
+            })
+            $('.psyhBlock .info-l').css({
+                display:"block",
+                transition:".3s ease"
+            })
+            $('.psyhBlock .info-arrow').css({
+                left:"-19px"
+            })
+            $('.psyhBlock .info-l').css({
+                display:"flex",
+                transform: "scale(1,1) translate(-155px, 17px)",
+            })
+            $('.psyhBlock .info-l .info__l1').css({
+                display:"block",
+
+            })
+        }, function () {
+            $('.psyhBlock .info-arrow').css({
+                right:"-19px"
+            })
+            $('.psyhBlock .info-l').css({
+                display:"flex",
+                transform: "scale(0,0) translate(-155px, 17px)"
+            })
+        })
+    $('.psyhBlock .bottom .bottom__list .bottom__list__item:nth-child(2) .item--name--left').hover(
+        function () {
+            $('.info__l1,.info__l2,.info__l3,.info__l4,.info__l5,.info__l6,info-arrow').css({
+                display:"none"
+            })
+            $('.psyhBlock .info-l').css({
+                display:"block",
+                transition:".3s ease"
+            })
+            $('.psyhBlock .info-arrow').css({
+                right:"-19px"
+            })
+            $('.psyhBlock .info-l').css({
+                display:"flex",
+                transform: "scale(1,1) translate(-155px, 27%)",
+            })
+            $('.psyhBlock .info-l .info__l2').css({
+                display:"block",
+
+            })
+        }, function () {
+            $('.psyhBlock .info-arrow').css({
+                right:"-19px"
+            })
+            $('.psyhBlock .info-l').css({
+                display:"none",
+                transform: "scale(0.3,0.3) translate(-155px, 27%)"
+            })
+        })
+    $('.psyhBlock .bottom .bottom__list .bottom__list__item:nth-child(3) .item--name--left').hover(
+        function () {
+            $('.info__l1,.info__l2,.info__l3,.info__l4,.info__l5,.info__l6,info-arrow').css({
+                display:"none"
+            })
+            $('.psyhBlock .info-l').css({
+                display:"block",
+                transition:".3s ease"
+            })
+            $('.psyhBlock .info-arrow').css({
+                right:"-19px"
+            })
+            $('.psyhBlock .info-l').css({
+                display:"flex",
+                transform: "scale(1,1) translate(-155px, 46%)",
+            })
+            $('.psyhBlock .info-l .info__l3').css({
+                display:"block",
+
+            })
+        }, function () {
+            $('.psyhBlock .info-arrow').css({
+                right:"-19px"
+            })
+            $('.psyhBlock .info-l').css({
+                display:"none",
+                transform: "scale(0.3,0.3) translate(-155px, 46%)"
+            })
+        })
+    $('.psyhBlock .bottom .bottom__list .bottom__list__item:nth-child(4) .item--name--left').hover(
+        function () {
+            $('.info__l1,.info__l2,.info__l3,.info__l4,.info__l5,.info__l6,info-arrow').css({
+                display:"none"
+            })
+            $('.psyhBlock .info-l').css({
+                display:"block",
+                transition:".3s ease"
+            })
+            $('.psyhBlock .info-arrow').css({
+                right:"-19px"
+            })
+            $('.psyhBlock .info-l').css({
+                display:"flex",
+                transform: "scale(1,1) translate(-155px, 65%)",
+            })
+            $('.psyhBlock .info-l .info__l4').css({
+                display:"block",
+
+            })
+        }, function () {
+            $('.psyhBlock .info-arrow').css({
+                right:"-19px"
+            })
+            $('.psyhBlock .info-l').css({
+                display:"flex",
+                transform: "scale(0,0) translate(-155px, 65%)"
+            })
+        })
+    $('.psyhBlock .bottom .bottom__list .bottom__list__item:nth-child(5) .item--name--left').hover(
+        function () {
+            $('.info__l1,.info__l2,.info__l3,.info__l4,.info__l5,.info__l6,info-arrow').css({
+                display:"none"
+            })
+            $('.psyhBlock .info-l').css({
+                display:"block",
+                transition:".3s ease"
+            })
+            $('.psyhBlock .info-arrow').css({
+                right:"-19px"
+            })
+            $('.psyhBlock .info-l').css({
+                display:"flex",
+                transform: "scale(1,1) translate(-155px, 84%)",
+            })
+            $('.psyhBlock .info-l .info__l5').css({
+                display:"block",
+
+            })
+        }, function () {
+            $('.psyhBlock .info-arrow').css({
+                right:"-19px"
+            })
+            $('.psyhBlock .info-l').css({
+                display:"flex",
+                transform: "scale(0,0) translate(-155px, 84%)"
+            })
+        })
+    $('.psyhBlock .bottom .bottom__list .bottom__list__item:nth-child(6) .item--name--left').hover(
+        function () {
+            $('.info__l1,.info__l2,.info__l3,.info__l4,.info__l5,.info__l6,info-arrow').css({
+                display:"none"
+            })
+            $('.psyhBlock .info-l').css({
+                display:"block",
+                transition:".3s ease"
+            })
+            $('.psyhBlock .info-arrow').css({
+                right:"-19px"
+            })
+            $('.psyhBlock .info-l').css({
+                display:"flex",
+                transform: "scale(1,1) translate(-155px, 103%)",
+            })
+            $('.psyhBlock .info-l .info__l6').css({
+                display:"block",
+
+            })
+        }, function () {
+            $('.psyhBlock .info-arrow').css({
+                right:"-19px"
+            })
+            $('.psyhBlock .info-l').css({
+                display:"flex",
+                transform: "scale(0,0) translate(-155px, 103%)"
+            })
+        })
+
     $(function () {
         var handle = $("#custom-handle");
         $("#slider").slider({
@@ -15,7 +365,7 @@ $(document).ready(function () {
                 if(val > 79){
                     $('.psyhBlock .bottom__list .bottom__list__item:nth-child(4) .item--name--right').css({
                         transaction : ".3s ease",
-                        fontSize : "1.1rem",
+                        fontSize : "1.25rem",
                         fontWeight : "600",
                     })
                 }
@@ -86,4 +436,5 @@ $(document).ready(function () {
 
         });
     });
+
 });
